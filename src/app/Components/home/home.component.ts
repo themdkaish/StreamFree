e9224cfaf8560c8b0f49a0efddb5d0765edf2241
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-08-30 12:32:00 +0530

    relative path

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit{
   }
   getIndianMovies() {
     this.http
-      .get('http://localhost:4200/assets/data/trending-movies.json')
+      .get('/assets/data/trending-movies.json')
       .subscribe((movies) => {
         this.trendingMovies = movies;
         console.log(this.trendingMovies)
